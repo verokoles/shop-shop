f1cf757763e75bbbc8974fc54926b9e46a89d46e
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-06-29 10:53:13 -0500

    cart

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Cart from '../components/Cart';
 
 const Home = () => {
   return (
     <div className="container">
-      <CategoryMenu />
-      <ProductList />
-    </div>
+  <CategoryMenu />
+  <ProductList />
+  <Cart />
+</div>
   );
 };
 
